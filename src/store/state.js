36b41a237eb3eb0a8305b9602efd1f335de97db3
alpsenell <alp@useinsider.com>
2021-03-09 15:30:38 +0300

    Startoff

---
+++ src/store/state.js
@@ -0,0 +1,5 @@
+export default {
+  allPlayers: [],
+  selectedPlayers: [],
+  substitutes: [],
+};
---
+++ src/store/types.js
@@ -0,0 +1,10 @@
+export const Action = {
+  FETCH_CLUB_PLAYERS: 'fetchClubPlayers',
+};
+
+export const Mutation = {
+  SET_CLUB_PLAYERS: 'setClubPlayers',
+  ADD_PLAYER_TO_LINEUP: 'addPlayerToLineup',
+  REMOVE_PLAYER_FROM_LINEUP: 'removePlayerFromLineup',
+  ADD_SUBSTITUTE: 'addSubstitute',
+};
